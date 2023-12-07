791ea3b0d15529d1a7aa676604a5f7c9260e1968
YohanJK07 <152532506+YohanJK07@users.noreply.github.com>
2023-12-06 23:55:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 //http://www.findsounds.com/ISAPI/search.dll
 //playSound('woof');
 
-function playSound(id) { //play sound
-    if (document.getElementById(id)) {
-        var sound = document.getElementById(id); //setup audio
-        sound.currentTime = 0; //reset position of playback to zero  //sound.load();
-        sound.play();
-    }
-}
+// function playSound(id) { //play sound
+//     if (document.getElementById(id)) {
+//         var sound = document.getElementById(id); //setup audio
+//         sound.currentTime = 0; //reset position of playback to zero  //sound.load();
+//         sound.play();
+//     }
+// }
+
+var button = document.getElementById('Paino'); // 
+
+button.addEventListener('click', myPlay); 
+
+function myPlay() {
+    var a = document.getElementById('Paino');
+    a.play();
+} ;
