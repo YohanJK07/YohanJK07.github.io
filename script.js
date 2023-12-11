e612d72b4acc67a5cd645a69d160395936a9940d
YohanJK07 <152532506+YohanJK07@users.noreply.github.com>
2023-12-11 11:48:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+//Piano setup
 var button1 = document.getElementById('Piano');
     var a = document.getElementById('pianoAudio');
 
+//Guitar setup
 var button2 = document.getElementById('Guiter');
     var b = document.getElementById('guitarAudio');
 
+//Drums setup
 var button3 = document.getElementById('Droms');
     var c = document.getElementById('drumAudio');
 
@@ -12,15 +15,17 @@ function myPlay() {
     a.play();
 } ;
 
-
+//Piano button
 document.getElementById('Piano').addEventListener("click", function() {
   a.play();
 });
 
+//Guitar button
 document.getElementById('Guiter').addEventListener("click", function() {
   b.play();
 });
 
+//Drums button
 document.getElementById('Droms').addEventListener("click", function() {
   c.play();
 });
