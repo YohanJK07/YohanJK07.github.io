ae3fcb47bd9afc877100cb6e5d0ab8025eca4823
YohanJK07 <152532506+YohanJK07@users.noreply.github.com>
2024-05-30 11:33:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight - 300);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 var cube = [];
